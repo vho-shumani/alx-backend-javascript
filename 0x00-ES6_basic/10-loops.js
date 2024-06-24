6988a2097604b0bceeaecb98d0740220ffe721c2
shumani <shumanisimeli@gmail.com>
2024-06-25 01:10:18 +0200

    created 10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    const value = idx;
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
