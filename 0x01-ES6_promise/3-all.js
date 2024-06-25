c3ef8142630a1628fda7542671c56e75b3bbc685
shumani <shumanisimeli@gmail.com>
2024-06-26 00:49:15 +0200

    updated 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promise = uploadPhoto();
-  const promise2 = createUser();
-
-  Promise.all([promise, promise2])
+  Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const [photo, user] = values;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
