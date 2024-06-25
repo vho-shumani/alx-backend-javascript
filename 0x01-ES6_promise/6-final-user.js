9081e4b20a2bf9b9881de30a80c166f37f04d7cd
shumani <shumanisimeli@gmail.com>
2024-06-25 22:52:04 +0200

    created 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values.map((value) => ({
+      status: value.status,
+      value: value.status === 'fulfilled' ? value.value : value.reason,
+    })));
+}
