06018d53344e044843a761b74e2b5b3c4246fd7a
shumani <shumanisimeli@gmail.com>
2024-06-26 01:23:33 +0200

    updated 7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload]).then(value => {
-    return value;
-  });
+  return Promise.race([chinaDownload, USDownload]).then((value) => value);
 }
