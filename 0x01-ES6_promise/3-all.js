367a4ee2bd0dcd278640be7f40e9a389e782e03e
shumani <shumanisimeli@gmail.com>
2024-06-26 00:35:53 +0200

    updated 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,7 @@ export default function handleProfileSignup() {
   Promise.all([promise, promise2])
     .then((values) => {
       const [photo, user] = values;
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
