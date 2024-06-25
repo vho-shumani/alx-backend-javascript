d8f5596fe86fa9230548fd7c13d5e142db9b4fc7
shumani <shumanisimeli@gmail.com>
2024-06-26 00:03:53 +0200

    created 100-await.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,8 +3,9 @@ export default function guardrail (mathFunction) {
     try {
         queue.push(mathFunction());
     } catch (error) {
-        queue.push(error);
+        queue.push(`Error: ${error.message}`);
+    } finally {
+        queue.push('Guardrail was processed');
     }
-    queue.push('Guardrail was processed');
     return queue;
 }
