9f34dcd4546086be9a486bd5908cb4b42344a740
shumani <shumanisimeli@gmail.com>
2024-06-27 18:57:10 +0200

    fixed lint error on 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,17 +2,17 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
-    }else {
+    } else {
       throw new TypeError('Name must be a string');
     }
     if (typeof length === 'number') {
       this._length = length;
-    }else {
+    } else {
       throw new TypeError('Length must be number');
     }
-    if (students instanceof Array && students.every(student => typeof student === 'string')){
+    if (students instanceof Array && students.every((student) => typeof student === 'string')) {
       this._students = students;
-    }else {
+    } else {
       throw new TypeError('Student must be array of string');
     }
   }
@@ -21,32 +21,35 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  set name(name){
-    if (typeof name === 'string')
+  set name(name) {
+    if (typeof name === 'string') {
       this._name = name;
-    else
+    } else {
       throw new TypeError('Name must be string');
+    }
   }
 
   get length() {
     return this._length;
   }
 
-  set length(length){
-    if (typeof length === 'number')
+  set length(length) {
+    if (typeof length === 'number') {
       this._length = length;
-    else
+    } else {
       throw TypeError('Length must be number');
+    }
   }
 
   get students() {
     return this._students;
   }
 
-  set students(students){
-    if (Array.isArray(students) && students.every(student => typeof student === 'string'))
+  set students(students) {
+    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
       this._students = students;
-    else
-      throw TypeError('Students must be array of string')
+    } else {
+      throw TypeError('Students must be array of string');
+    }
   }
 }
