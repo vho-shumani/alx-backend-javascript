c2a050c6f59f98f5152bdd064eb7a634fa25c1dc
shumani <shumanisimeli@gmail.com>
2024-06-27 22:40:43 +0200

    created 6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft = 0, floors = 0) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return super.sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
