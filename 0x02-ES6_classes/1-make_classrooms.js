f51060dd36ad229b76b1ddf1feab96cc476d52a6
shumani <shumanisimeli@gmail.com>
2024-06-27 16:40:51 +0200

    created 1-main.js and 1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from "./0-classroom";
+export default function initializeRooms() {
+  const firstClass = new ClassRoom(19);
+  const secondClass = new ClassRoom(20);
+  const thirdClass = new ClassRoom(34); 
+  return [firstClass, secondClass, thirdClass];
+}
