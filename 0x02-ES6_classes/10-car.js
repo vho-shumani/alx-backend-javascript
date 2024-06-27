ff53511c347323949efa68f1ddac0c1631b09bfe
shumani <shumanisimeli@gmail.com>
2024-06-28 01:00:53 +0200

    created 10-car.js
    mend

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,11 @@
-const clone = Symbol('clone')
+const clone = Symbol('clone');
 export default class Car {
-  constructor(brand, motor, color){
+  constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
-  
+
   static get [Symbol.species]() {
     return this;
   }
