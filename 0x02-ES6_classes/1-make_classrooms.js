2bef994705e3c4dbcee231727cc10706e0df1e4b
shumani <shumanisimeli@gmail.com>
2024-06-27 16:43:22 +0200

    update 1-make_classrooms.js files

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
   const firstClass = new ClassRoom(19);
   const secondClass = new ClassRoom(20);
-  const thirdClass = new ClassRoom(34); 
+  const thirdClass = new ClassRoom(34);
   return [firstClass, secondClass, thirdClass];
 }
