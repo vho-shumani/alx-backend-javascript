f41ea426a25a8f0d38512a61967f10eeb12c2b3e
shumani <shumanisimeli@gmail.com>
2024-06-27 18:19:34 +0200

    completed task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    }else {
+      throw new TypeError('Length must be number');
+    }
+    if (students instanceof Array && students.every(student => typeof student === 'string')){
+      this._students = students;
+    }else {
+      throw new TypeError('Student must be array of string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name){
+    if (typeof name === 'string')
+      this._name = name;
+    else
+      throw new TypeError('Name must be string');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length){
+    if (typeof length === 'number')
+      this._length = length;
+    else
+      throw TypeError('Length must be number');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students){
+    if (Array.isArray(students) && students.every(student => typeof student === 'string'))
+      this._students = students;
+    else
+      throw TypeError('Students must be array of string')
+  }
+}
