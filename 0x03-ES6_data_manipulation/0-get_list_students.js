6bf796b60327dbcd2070f9223df8c82cea80db4b
Shumani <shumanisimeli@gmail.com>
2024-07-02 18:57:11 +0200

    created 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,7 @@
 export default function getListStudents() {
-  const obj1 = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
-
-  const obj2 = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-
-  const obj3 = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-
-  return [obj1, obj2, obj3];
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
