883f9598c6b58a3b6ac9b1a3c6bfd49ca8a29f44
Shumani <shumanisimeli@gmail.com>
2024-07-03 00:45:28 +0200

    created 100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const queryCount = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, queryCount);
+
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
