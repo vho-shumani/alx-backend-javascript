8ba3b6c04f891e4cbe9f29ed69fbd4bb26470345
shumani <shumanisimeli@gmail.com>
2024-08-20 15:12:00 +0200

    updated  2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,19 +2,23 @@ const fs = require('fs');
 function countStudents(path) {
     let cs = [];
     let swe = [];
-
-    data = fs.readFileSync(path, 'utf8').trim().split('\n').slice(1);
-    for (line of data){
-        categories = line.split(',');
-        if (categories[3] === 'CS') {
-            cs.push(categories[0]);
+    try{
+        data = fs.readFileSync(path, 'utf8').trim().split('\n').slice(1);
+        for (line of data){
+            categories = line.split(',');
+            if (categories[3] === 'CS') {
+                cs.push(categories[0]);
+            }
+            else {
+                swe.push(categories[0]);
+            }
         }
-        else {
-            swe.push(categories[0]);
-        }
+        console.log(`Number of students: ${data.length}`);
+        console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+        console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
     }
-    console.log(`Number of students: ${data.length}`);
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+    catch {
+        console.log('Cannot load the database');
+    }
 }
 module.exports = countStudents;
