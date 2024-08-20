2b131bbd2bcdbf9ea1bffe16f78af13c6e7d7e99
shumani <shumanisimeli@gmail.com>
2024-08-20 19:59:12 +0200

    updated 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,22 +1,25 @@
 const { error } = require('console');
 const fs = require('fs');
 function countStudents(path) {
-
-    const data = fs.readFileSync(path, 'utf8').trim().split('\n').slice(1);
-    const fields = {};
-    console.log(`Number of students: ${data.length}`);
-    for (line of data) {   
-        categories = line.split(',');
-        const field = categories[3];
-        if (!fields[field]) {
-            fields[field] = {count: 0, list: []};
+    try {
+        const data = fs.readFileSync(path, 'utf8').trim().split('\n').slice(1);
+        const fields = {};
+        console.log(`Number of students: ${data.length}`);
+        for (line of data) {   
+            categories = line.split(',');
+            const field = categories[3];
+            if (!fields[field]) {
+                fields[field] = {count: 0, list: []};
+            }
+            fields[field].count += 1;
+            fields[field].list.push(categories[0]);
         }
-        fields[field].count += 1;
-        fields[field].list.push(categories[0])
+        for (let fieldKey in fields) {
+            console.log(`Number of students in ${fieldKey}: ${fields[fieldKey].count}. List: ${fields[fieldKey].list.join(', ')}`);
+        }
     }
-    for (let fieldKey in fields) {
-        console.log(`Number of students in ${fieldKey}: ${fields[fieldKey].count}. List: ${fields[fieldKey].list.join(', ')}`);
+    catch {
+        throw new Error('Cannot load the database');
     }
-
 }
 module.exports = countStudents;
