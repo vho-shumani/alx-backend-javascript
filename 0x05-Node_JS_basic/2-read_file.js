318901adac48be65e71beaa59a930ce807d8229e
shumani <shumanisimeli@gmail.com>
2024-08-20 20:31:40 +0200

    updated 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 function countStudents(path) {
     try {
-        const data = fs.readFileSync(path, 'utf8').trim().split('\n').slice(1);
+        const data = fs.readFileSync(path, 'utf8')
+            .trim()
+            .split('\n')
+            .slice(1)
+            .filter(line => line.trim() !== '');
         const fields = {};
         console.log(`Number of students: ${data.length}`);
         for (line of data) {   
@@ -16,7 +20,7 @@ function countStudents(path) {
         for (let fieldKey in fields) {
             console.log(`Number of students in ${fieldKey}: ${fields[fieldKey].count}. List: ${fields[fieldKey].list.join(', ')}`);
         }
-    } catch {
+    } catch (err) {
         throw new Error('Cannot load the database');
     }
 }
