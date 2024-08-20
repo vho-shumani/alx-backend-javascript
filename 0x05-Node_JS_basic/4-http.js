a077da68e78eb307755fbc831101dbe13adf4948
shumani <shumanisimeli@gmail.com>
2024-08-20 21:46:33 +0200

    created 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const { response } = require('express');
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+    response.end('Hello Holberton School!');
+})
+
+app.listen(1245);
+
+module.exports = app
