06feec7c0a4fce44f29922393e3642f20347ab68
shumani <shumanisimeli@gmail.com>
2024-08-23 01:43:43 +0200

    update

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,3 +1,5 @@
+const Utils = require('./utils');
+
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
     const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${result}`);
