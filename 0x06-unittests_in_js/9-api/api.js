515b8d939eff5b5d4a60d8560f466854f5162337
shumani <shumanisimeli@gmail.com>
2024-08-23 02:15:05 +0200

    created 9-api

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
+});
+
+app.listen(7865);
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+
+describe('Cart page', () => {
+  it('should return correct status code when :id is a number', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return 404 when :id is NOT a number', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+});
