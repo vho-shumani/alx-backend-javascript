a2cb033ff9cc6d0191fd8b6c1aa8be4002f77dfb
shumani <shumanisimeli@gmail.com>
2024-08-23 20:13:32 +0200

    created 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8')
+      .trim()
+      .split('\n')
+      .slice(1)
+      .filter(line => line.trim() !== '');
+      const fields = {};
+      console.log(`Number of students: ${data.length}`);
+      for (line of data) {
+        categories = line.split(',');
+        const field = categories[3]
+        if (!fields[field]) {
+            fields[field] = {count: 0, list: []}
+        }
+        fields[field].count += 1;
+        fields[field].list.push(categories[0]);
+      }
+      for (let fieldKey in fields) {
+        console.log(`Number of students in ${fieldKey}: ${fields[fieldKey].count}. List: ${fields[fieldKey].list.join(', ')}`)
+      }
+  }
+  catch {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
