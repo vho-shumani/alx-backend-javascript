7f37e6b613936bab986de7386c62a925e1e4cca8
shumani <shumanisimeli@gmail.com>
2024-08-23 20:33:18 +0200

    fixed lint errors

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,25 +6,25 @@ function countStudents(path) {
       .trim()
       .split('\n')
       .slice(1)
-      .filter(line => line.trim() !== '');
-      const fields = {};
-      console.log(`Number of students: ${data.length}`);
-      for (line of data) {
-        categories = line.split(',');
-        const field = categories[3]
-        if (!fields[field]) {
-            fields[field] = {count: 0, list: []}
-        }
-        fields[field].count += 1;
-        fields[field].list.push(categories[0]);
+      .filter((line) => line.trim() !== '');
+    const fields = {};
+    console.log(`Number of students: ${data.length}`);
+    for (const line of data) {
+      const categories = line.split(',');
+      const field = categories[3];
+      if (!fields[field]) {
+        fields[field] = { count: 0, list: [] };
       }
-      for (let fieldKey in fields) {
-        console.log(`Number of students in ${fieldKey}: ${fields[fieldKey].count}. List: ${fields[fieldKey].list.join(', ')}`)
+      fields[field].count += 1;
+      fields[field].list.push(categories[0]);
+    }
+    for (const fieldKey in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, fieldKey)) {
+        console.log(`Number of students in ${fieldKey}: ${fields[fieldKey].count}. List: ${fields[fieldKey].list.join(', ')}`);
       }
-  }
-  catch {
+    }
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
-
 module.exports = countStudents;
