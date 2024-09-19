89a2093ed127395e9eda7b52d5cdf74be305968a
shumani <shumani@gmail.com>
2024-09-19 16:37:09 +0200

    removed direcrory

--- 0x05-Node_JS_basic/4-http.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const app = http.createServer((request, response) => {
-  response.end('Hello Holberton School!');
-});
-
-app.listen(1245);
-
-module.exports = app;
--- 0x06-unittests_in_js/8-api/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
-
-app.listen(7865);
