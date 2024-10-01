e1ea829f5623483938911f38f1d3c1e1d14ebd1f
shumani <shumanisimeli@gmail.com>
2024-10-01 21:20:32 +0200

    updated 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,6 @@
-process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', function() {
   const chunk = process.stdin.read();
