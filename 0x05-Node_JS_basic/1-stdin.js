1ab5962694ba09682e79947d9bab482199d108b6
shumani <shumanisimeli@gmail.com>
2024-10-01 19:48:52 +0200

    created 1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding('utf8');
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', function() {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+    }
+  });
+
+process.stdin.on('end', function() {
+  console.log('This important software is now closing');
+});
