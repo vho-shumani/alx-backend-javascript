8473d0a706c22e109cafbf6c79f5275d617a4d3f
shumani <shumanisimeli@gmail.com>
2024-10-02 03:09:55 +0200

    updated 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req,res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-})
+});
 
 app.listen(1245);
