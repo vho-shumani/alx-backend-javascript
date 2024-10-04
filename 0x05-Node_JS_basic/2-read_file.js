f1d27bb72c387e4363eeb02ee4aac6b1d81ba390
shumani <shumanisimeli@gmail.com>
2024-10-04 22:07:12 +0200

    fixed eslint errors

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,7 +4,7 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.trim().split('\n');
-    const students = lines.slice(1).filter(line => line.trim() !== '');
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
     const numberOfStudents = students.length;
 
     console.log(`Number of students: ${numberOfStudents}`);
