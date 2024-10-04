fb1b15d9db3b15c87b40b8b2f400acaafa9c7b9f
shumani <shumanisimeli@gmail.com>
2024-10-04 21:47:39 +0200

    created 4-payment.js and 4-payment.test.js

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should stub Utils.calculateNumber and verify console.log', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
